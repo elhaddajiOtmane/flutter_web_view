ae77f74395656fb5ac11a285e87de1bc4beda2a6
ELHADADJI Otmane <39245156+elhaddajiOtmane@users.noreply.github.com>
2024-01-14 13:59:09 +0100

    First commit

---
+++ android/app/src/main/kotlin/com/webnexa/web_views/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.webnexa.web_views
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WebView Example',
+      debugShowCheckedModeBanner: false,  // Removes the debug banner
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: WebViewExample(),
+    );
+  }
+}
+
+class WebViewExample extends StatefulWidget {
+  @override
+  _WebViewExampleState createState() => _WebViewExampleState();
+}
+
+class _WebViewExampleState extends State<WebViewExample> {
+  late WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Instantiate the WebViewController
+    _controller = WebViewController();
+
+    // Set up the controller
+    _controller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (String url) {
+            print('Page started loading: $url');
+          },
+          onPageFinished: (String url) {
+            print('Page finished loading: $url');
+          },
+          onWebResourceError: (WebResourceError error) {
+            print('Web resource error: $error');
+          },
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://hdgoodies.fr')); // Replace with your website URL
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Removed AppBar to remove the title, or you can just remove the title property
+      body: WebViewWidget(controller: _controller),
+    );
+  }
+}
